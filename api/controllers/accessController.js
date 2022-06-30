efff0f6db8c93877d5cac5474edaf3687d76e773
Harvey Magana <heyharv2012@gmail.com>
2022-06-30 16:53:51 -0700

    Removed the old accessController logic.

--- api/controllers/accessController.js
+++ api/controllers/accessController.js
@@ -1,30 +1,3 @@
-const { roles } = require('../utils/roles');
-
-exports.grantAccess = function(action, resource) {
-	return async (req, res, next) => {
-		try {
-			const data = req.user;
-      
-			let modAction = action;
-
-			if(data.id === req.body.user_id) {
-				modAction = action.replace('Any', 'Own');
-			}
-
-			const permission = roles.can(data.role)[modAction](resource);
-
-			if(!permission.granted) {
-				return res.status(401).json({
-					error: 'You do not have the necessary permission to perform this action.'
-				});
-			}
-			next();
-		} catch (error) {
-			next(error);
-		}
-	};
-};
-
 exports.allowIfLoggedin = async (req, res, next) => {
 	try {
 		const user = await req.session.verified;
